e7b016e006337b113d9ce81b3b10a7d71a57c777
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2023-04-13 19:03:38 -0300

    chore(backend): refactor folder structure

--- server/src/main/adapters/ExpressAdapter.ts
+++ server/src/main/adapters/ExpressAdapter.ts
@@ -1,5 +1,5 @@
 import { type Request, type Response } from 'express';
-import { type Controller, type HttpRequest, type HttpResponse } from '../presentation/protocols';
+import { type Controller, type HttpRequest, type HttpResponse } from '../../presentation/protocols';
 
 export const adaptRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
